32226d50a6fa58e9c64c7c990be75755d038aace
Vladimir Striukov <vladimir.alexeevich.morrowind@gmail.com>
2019-03-26 20:19:00 +0300

    testing images

--- adv_board/serializers.py
+++ adv_board/serializers.py
@@ -27,7 +27,7 @@ class AnnouncementSerializer(serializers.ModelSerializer):
         fields = ('id', 'title', 'content', 'price', 'bargain', 'created_on', 'category', 'images',)
 
     @classmethod
-    def images_per_instance_validator(cls, pk, path_list, image_paths):
+    def images_per_instance_validator(cls, pk, path_list, image_paths, for_update=False):
 
         # check if new list of urls contains any and not exceeding the limit
         if len(path_list) > ANNOUNCEMENT_IMAGE_LIMIT:
@@ -35,25 +35,31 @@ class AnnouncementSerializer(serializers.ModelSerializer):
                 len(path_list), ANNOUNCEMENT_IMAGE_LIMIT
             ))
         # check if url ends with image extension
+        print(path_list)
         image_list = list()
         for path in path_list:
             if not any([path.endswith(e) for e in ImagePath.valid_extensions]):
                 raise ValidationError("Incorrect image format, use one of {}".format(
                     ImagePath.valid_extensions,
                 ))
-            if path not in image_paths:
-                image_list.append(ImagePath(announcement_id=pk, path=path))
+            if not for_update:
+                if path not in image_paths:
+                    print('path ok')
+                    image_list.append(ImagePath(announcement_id=pk, path=path))
+                else:
+                    # Duplicate images won't be inserted again
+                    pass
             else:
-                # Duplicate images won't be inserted again
-                pass
+                image_list.append(ImagePath(announcement_id=pk, path=path))
         return image_list
 
     @classmethod
-    def multiple_image_instances_prepare(cls, images, pk):
+    def multiple_image_instances_prepare(cls, images, pk, for_update=False):
         path_list = [arg['path'] for arg in images if 'path' in arg]
         images = ImagePath.objects.filter(announcement_id=pk)
         image_paths = images.values_list('path', flat=True).order_by('id')
-        images = cls.images_per_instance_validator(pk=pk, path_list=path_list, image_paths=image_paths)
+        images = cls.images_per_instance_validator(pk=pk, path_list=path_list,
+                                                   image_paths=image_paths, for_update=for_update)
         return images
 
     def create(self, validated_data):
@@ -63,6 +69,23 @@ class AnnouncementSerializer(serializers.ModelSerializer):
         images = self.multiple_image_instances_prepare(images, obj.id)
         for image in images:
             image.save()
+        return obj
+
+    def update(self, instance, validated_data):
+        images = None
+        if 'images' in validated_data:
+            images = validated_data.pop('images')
+        obj = super().update(instance, validated_data)
+
+        # basically removes all images and replace with new ones, if new list exists and valid
+        try:
+            validated_images = self.multiple_image_instances_prepare(images, obj.id, for_update=True)
+            ImagePath.objects.filter(announcement_id=obj.id).delete()
+            for image in validated_images:
+                image.save()
+        except ValidationError as e:
+            raise e
+
         return obj
 
 
--- adv_board/tests/test_announcements.py
+++ adv_board/tests/test_announcements.py
@@ -3,17 +3,25 @@ from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
 
-from ..models import Announcement, Category
+from ..models import Announcement, Category, ImagePath
 from ..serializers import CategorySerializer, AnnouncementSerializer
 from django.db.models import F
+import json
 
 
-def create_adv(title='Test', category=None,
-               bargain=False, price=5551.55,
-               content=None,):
+SAMPLE_FILE_LIST = (
+    'https://farm3.staticflickr.com/2912/13981352255_fc59cfdba2_b.jpg',
+    'https://homepages.cae.wisc.edu/~ece533/images/airplane.png',
+    'https://homepages.cae.wisc.edu/~ece533/images/arctichare.png',
+    'https://homepages.cae.wisc.edu/~ece533/images/baboon.png',
+    'https://homepages.cae.wisc.edu/~ece533/images/boat.png',
+    'https://homepages.cae.wisc.edu/~ece533/images/sails.png',
+    'https://homepages.cae.wisc.edu/~ece533/images/monarch.png',
+    'https://homepages.cae.wisc.edu/~ece533/images/peppers.png'
+)
 
-    if content is None:
-        content = '''Lorem ipsum dolor sit amet, consectetur adipiscing 
+
+LOREM_CONTENT = '''Lorem ipsum dolor sit amet, consectetur adipiscing 
                      elit. Quisque accumsan ullamcorper leo. Nullam malesuada 
                      elit et laoreet consectetur. Proin sodales ullamcorper 
                      laoreet. Mauris magna ligula, volutpat non est in, sodales 
@@ -25,13 +33,42 @@ def create_adv(title='Test', category=None,
                      hendrerit nisi. Ut posuere cursus accumsan. Sed eget euismod tellus
                      '''
 
-    return Announcement.objects.create(title=title, category_id=category.id,
-                                       bargain=bargain, price=price, content=content, )
+
+def create_adv(title='Test', category=None,
+               bargain=False, price=5551.55,
+               content=None, file_list=None, commit=True):
+
+    if file_list is None:
+        file_list = SAMPLE_FILE_LIST
+
+    if content is None:
+        content = LOREM_CONTENT
+
+    new_ann = Announcement.objects.create(title=title, category_id=category.id,
+                                          bargain=bargain, price=price, content=content)
+
+    image_list = list()
+    for path in file_list:
+        image_list.append(ImagePath(path=path, announcement_id=new_ann.id))
+
+    ImagePath.objects.bulk_create(image_list)
+
+    return new_ann
 
 
 class BaseViewTest(APITestCase):
     client = APIClient()
     fixtures = ('category.json', )
+
+    additional_images = (
+        'https://homepages.cae.wisc.edu/~ece533/images/tulips.png',
+        'https://homepages.cae.wisc.edu/~ece533/images/watch.png',
+        'https://cdn.arstechnica.net/wp-content/uploads/2017/03/GettyImages-461246108-1-800x941.jpg'
+    )
+
+    non_image_url = (
+        'https://simpleisbetterthancomplex.com/tutorial/2016/07/28/how-to-create-django-signals.html',
+    )
 
     def setUp(self):
         # create a new leaf category
@@ -49,7 +86,7 @@ class GetAnnouncementsTest(BaseViewTest):
     """
     def test_announcement_parent_categories(self):
         response = self.client.get(
-            reverse('adv-detail', kwargs={'pk': 1}),
+            reverse('adv-detail', kwargs={'pk': self.adv.id}),
         )
         expected = Announcement.objects.get(title=self.test_adv_name)
         serialized = AnnouncementSerializer(expected)
@@ -66,3 +103,66 @@ class GetAnnouncementsTest(BaseViewTest):
             self.assertEqual(ad, self.adv)
 
 
+class CreateNewAnnouncement(APITestCase):
+    """ Test module for inserting a new puppy """
+
+    def setUp(self):
+        self.client = APIClient()
+        base_category = Category.objects.filter(lft=F('rght')-1).first()
+        self.leaf_category = Category.objects.create(name='brand new category', parent=base_category)
+
+        self.image_list = list()
+        for image in SAMPLE_FILE_LIST:
+            self.image_list.append({'path': image})
+        print(self.image_list)
+
+        self.valid_payload = {
+            "title": "george lukas",
+            "content": "impossibly dumb content",
+            "price": 200,
+            "bargain": True,
+            "category": self.leaf_category.name,
+            "images": self.image_list
+        }
+        # self.invalid_payload = {
+        #     'name': '',
+        #     'age': 4,
+        #     'breed': 'Pamerion',
+        #     'color': 'White'
+        # }
+    """
+    Ensure that all crud operations for announcement works
+    """
+    def test_create_valid_puppy(self):
+        response = self.client.post(
+            reverse('adv-list'),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json'
+        )
+        print(json.dumps(self.valid_payload))
+        print(response.data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+    # def test_create_new_adv_successful(self):
+    #     ad_num = Announcement.objects.all().count()
+    #     test_ad = create_adv(category=self.leaf_category)
+    #     # {
+    #     #     "title": "string",
+    #     #     "content": "string",
+    #     #     "price": 0,
+    #     #     "bargain": true,
+    #     #     "category": "string",
+    #     #     "images": [
+    #     #         "string"
+    #     #     ]
+    #     # }
+    #     response = self.client.post(
+    #         reverse('adv-list', kwargs={'title': 'crud added'})
+    #     )
+    #
+    #     print(response.data)
+    #     self.assertEqual(ad_num + 1, Announcement.objects.all().count())
+    #
+    #     response = self.client.post
+    # pass
